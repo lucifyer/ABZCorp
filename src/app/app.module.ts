e8b95b71039b8635aebaabc03834c5e0f7cd0b50
lucifyer <gawasvishnu26@gmail.com>
2018-10-22 00:24:59 +0530

    table with sort working, add member working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LeftComponentComponent } from './left-component/left-component.component';
 import { RightComponentComponent } from './right-component/right-component.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LeftComponentComponent,
-    RightComponentComponent
-  ],
+  declarations: [AppComponent, RightComponentComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatSortModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/right-component/right-component.component.ts
+++ src/app/right-component/right-component.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { MatSort, MatTableDataSource } from '@angular/material';
 
 @Component({
   selector: 'app-right-component',
@@ -8,10 +10,50 @@ import { Component, OnInit, Input } from '@angular/core';
 export class RightComponentComponent implements OnInit {
   public employees = [
     {
-      firstName: 'Ted',
+      firstName: 'Dean',
+      lastName: 'Jones',
+      email: 'dean.jones@gmail.com',
+      phone: '+1 245 2456 234',
+      role: 'Sr. Mangager',
+      department: 'Design',
+      onlineStat: 0,
+      projectID: 1
+    },
+    {
+      firstName: 'Emily',
+      lastName: 'Clarke',
+      email: 'emily.clarke@gmail.com',
+      phone: '+1 245 2456 234',
+      role: 'Tech Lead',
+      department: 'Design',
+      onlineStat: 1,
+      projectID: 1
+    },
+    {
+      firstName: 'Gary',
+      lastName: 'Kristen',
+      email: 'gary.k@gmail.com',
+      phone: '+1 245 2456 234',
+      role: 'Engineer',
+      department: 'Marketing',
+      onlineStat: 1,
+      projectID: 1
+    },
+    {
+      firstName: 'Hamilton',
+      lastName: 'John',
+      email: 'abc@gmail.com',
+      phone: '+1 245 2456 234',
+      role: 'Mangager',
+      department: 'Admin',
+      onlineStat: 1,
+      projectID: 1
+    },
+    {
+      firstName: 'Kartik',
       lastName: 'Mosby',
       email: 'abc@gmail.com',
-      phone: '1232342342',
+      phone: '+1 245 2456 234',
       role: 'Mangager',
       department: 'IT',
       onlineStat: 1,
@@ -21,7 +63,7 @@ export class RightComponentComponent implements OnInit {
       firstName: 'Ted',
       lastName: 'Mosby',
       email: 'abc@gmail.com',
-      phone: '1232342342',
+      phone: '+1 245 2456 234',
       role: 'Mangager',
       department: 'IT',
       onlineStat: 1,
@@ -31,7 +73,7 @@ export class RightComponentComponent implements OnInit {
       firstName: 'Ted',
       lastName: 'Mosby',
       email: 'abc@gmail.com',
-      phone: '1232342342',
+      phone: '+1 245 2456 234',
       role: 'Mangager',
       department: 'IT',
       onlineStat: 1,
@@ -41,7 +83,7 @@ export class RightComponentComponent implements OnInit {
       firstName: 'Ted',
       lastName: 'Mosby',
       email: 'abc@gmail.com',
-      phone: '1232342342',
+      phone: '+1 245 2456 234',
       role: 'Mangager',
       department: 'IT',
       onlineStat: 1,
@@ -51,7 +93,7 @@ export class RightComponentComponent implements OnInit {
       firstName: 'Ted',
       lastName: 'Mosby',
       email: 'abc@gmail.com',
-      phone: '1232342342',
+      phone: '+1 245 2456 234',
       role: 'Mangager',
       department: 'IT',
       onlineStat: 1,
@@ -61,7 +103,7 @@ export class RightComponentComponent implements OnInit {
       firstName: 'Ted',
       lastName: 'Mosby',
       email: 'abc@gmail.com',
-      phone: '1232342342',
+      phone: '+1 245 2456 234',
       role: 'Mangager',
       department: 'IT',
       onlineStat: 1,
@@ -71,7 +113,7 @@ export class RightComponentComponent implements OnInit {
       firstName: 'Ted',
       lastName: 'Mosby',
       email: 'abc@gmail.com',
-      phone: '1232342342',
+      phone: '+1 245 2456 234',
       role: 'Mangager',
       department: 'IT',
       onlineStat: 1,
@@ -81,7 +123,7 @@ export class RightComponentComponent implements OnInit {
       firstName: 'Ted',
       lastName: 'Mosby',
       email: 'abc@gmail.com',
-      phone: '1232342342',
+      phone: '+1 245 2456 234',
       role: 'Mangager',
       department: 'IT',
       onlineStat: 1,
@@ -91,7 +133,7 @@ export class RightComponentComponent implements OnInit {
       firstName: 'Ted',
       lastName: 'Mosby',
       email: 'abc@gmail.com',
-      phone: '1232342342',
+      phone: '+1 245 2456 234',
       role: 'Mangager',
       department: 'IT',
       onlineStat: 1,
@@ -101,7 +143,7 @@ export class RightComponentComponent implements OnInit {
       firstName: 'Ted',
       lastName: 'Mosby',
       email: 'abc@gmail.com',
-      phone: '1232342342',
+      phone: '+1 245 2456 234',
       role: 'Mangager',
       department: 'IT',
       onlineStat: 1,
@@ -111,47 +153,7 @@ export class RightComponentComponent implements OnInit {
       firstName: 'Ted',
       lastName: 'Mosby',
       email: 'abc@gmail.com',
-      phone: '1232342342',
-      role: 'Mangager',
-      department: 'IT',
-      onlineStat: 1,
-      projectID: 1
-    },
-    {
-      firstName: 'Ted',
-      lastName: 'Mosby',
-      email: 'abc@gmail.com',
-      phone: '1232342342',
-      role: 'Mangager',
-      department: 'IT',
-      onlineStat: 1,
-      projectID: 1
-    },
-    {
-      firstName: 'Ted',
-      lastName: 'Mosby',
-      email: 'abc@gmail.com',
-      phone: '1232342342',
-      role: 'Mangager',
-      department: 'IT',
-      onlineStat: 1,
-      projectID: 1
-    },
-    {
-      firstName: 'Ted',
-      lastName: 'Mosby',
-      email: 'abc@gmail.com',
-      phone: '1232342342',
-      role: 'Mangager',
-      department: 'IT',
-      onlineStat: 1,
-      projectID: 1
-    },
-    {
-      firstName: 'Ted',
-      lastName: 'Mosby',
-      email: 'abc@gmail.com',
-      phone: '1232342342',
+      phone: '+1 245 2456 234',
       role: 'Mangager',
       department: 'IT',
       onlineStat: 1,
@@ -159,15 +161,77 @@ export class RightComponentComponent implements OnInit {
     }
   ];
 
+  displayedColumns: string[] = [
+    'firstName',
+    'department',
+    'email',
+    'phone',
+    'role'
+  ];
+  dataSource = new MatTableDataSource(this.employees);
+
+  @ViewChild(MatSort)
+  sort: MatSort;
+  addMember: FormGroup;
+  emailPattern: any = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+  phonePattern: any = /^(\+91[\-\s]?)?[0]?(91)?[789]\d{9}$/;
+  showModal: boolean = false;
+
   @Input()
   data: any;
   ngOnChanges(changes) {
     // changes.prop contains the old and the new value...
-    console.log(this.data);
+    // console.log(this.data);
     //Filter employees based on project id
   }
 
-  constructor() {}
+  displayModal() {
+    //Display the modal of form to add a new employee
+    this.showModal = true;
+  }
 
-  ngOnInit() {}
+  constructor(private fb: FormBuilder) {
+    this.addMember = this.fb.group({
+      firstName: [null, Validators.compose([Validators.required])],
+      lastName: [null, Validators.compose([Validators.required])],
+      phone: [
+        null,
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(this.phonePattern)
+        ])
+      ],
+      email: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(this.emailPattern)
+        ])
+      ],
+      role: [null, null],
+      department: [null, null]
+    });
+  }
+
+  memberRegister(data) {
+    console.log('\n\nForm Data: ', data);
+    let newMember = {
+      firstName: data.firstName,
+      lastName: data.lastName,
+      email: data.emailId,
+      phone: data.phone,
+      role: data.role,
+      department: data.department,
+      onlineStat: 1,
+      projectID: 1
+    };
+
+    this.employees.push(newMember);
+    this.dataSource.sort = this.sort;
+    this.showModal = false;
+  }
+
+  ngOnInit() {
+    this.dataSource.sort = this.sort;
+  }
 }
